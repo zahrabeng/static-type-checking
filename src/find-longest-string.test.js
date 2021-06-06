0d7247f4c79c8d5a24208b535080af3e3c1a4d16
Richard Ng <richardcrng@gmail.com>
2021-06-06 17:38:39 +0100

    Format test file with Prettier

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -1,11 +1,11 @@
 // import function from local file
-const findLongestString = require('./find-longest-string');
+const findLongestString = require("./find-longest-string");
 
-test('findLongestString finds the longest string in an array', () => {
+test("findLongestString finds the longest string in an array", () => {
   expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("nice");
   expect(findLongestString(["why", "hello", "to", "you"])).toBe("hello");
-})
+});
 
-test('findLongestString returns the earlier string in cases of joint longest strings', () => {
+test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
-})
+});
