0a96aa2011cbe83fab3b3c249157707b652c6069
Zahra <bghzahra@outlook.com>
2022-02-24 17:09:14 +0000

    initial commit

---
+++ src/find-shortest-string.ts
@@ -0,0 +1,18 @@
+/**
+ * Find the shortest string in an array.
+ * In the case of a tie, returns the earlier string in the array.
+ *
+ * @param stringArr array of strings
+ * @returns the shortest string
+ */
+ function findShortestString(stringArr: string[]): string {
+    let shortestString = stringArr[0];
+    for (const str of stringArr) {
+      if (str.length < shortestString.length) {
+        shortestString = str;
+      }
+    }
+    return shortestString.length;
+  }
+  
+  export default findShortestString;
